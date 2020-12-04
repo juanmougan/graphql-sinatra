062fc9425fe0b7297201f4f6c100a993caf7346c
Juan Manuel Mougan <juanmougan@gmail.com>
2020-12-03 23:05:08 -0300

    Deleted commented code

--- create_game.rb
+++ create_game.rb
@@ -10,18 +10,15 @@ class Mutations::CreateGame < Mutations::BaseMutation
   argument :first_player, String, required: false
   argument :second_player, String, required: false
   
-  #field :game, Game, null: true
   field :success, Boolean, null: true
   field :errors, [String], null: false
 
   # function to execute on mutation
-  # TODO this is falling back to the defaults
   def resolve(first_player:, second_player:)
     puts "Got first_player: #{first_player} and second_player: #{second_player}"
     game = Games.add(first_player, second_player)
     # No DB, shouldn't fail 🤷‍♂️
     return {
-      #game: game,
       success: true,
       errors: []
     }
